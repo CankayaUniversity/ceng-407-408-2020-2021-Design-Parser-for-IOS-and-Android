498495f1eef5a9697b793bd70a9e5d4a261c5ab5
Mustafa İpek <33962976+mistova@users.noreply.github.com>
2021-05-07 17:45:29 +0300

    Revert "Update AndroidMapAttribute.java"
    This reverts commit 5e2e9e3d0a0aa3c05724bebe5204e934cfe83d19.

--- com/AndroidMapAttribute.java
+++ com/AndroidMapAttribute.java
@@ -24,20 +24,13 @@ public class AndroidMapAttribute{
     private static final HashMap<String, BracketCounter> keywords;
     static {
         keywords = new HashMap<>();
-        //View
-        keywords.put("View_background-color" , new BracketCounter("setBackgroundColor(Color.parseColor(", 2));
-        keywords.put("View_gravity" , new BracketCounter("setGravity(GRAVITY.", 1));
-        keywords.put("View_width" , new BracketCounter("getLayoutParams().height = ", 0));
-        keywords.put("View_height" , new BracketCounter("getLayoutParams().width = ", 0));
-        keywords.put("View_orientation" , new BracketCounter("setOrientation(LinearLayout.", 1));
-        
-        //Text
-        keywords.put("Text_background-color" , new BracketCounter("setBackgroundColor(Color.parseColor(", 2));
-        keywords.put("Text_gravity" , new BracketCounter("setGravity(GRAVITY.", 1));
-        keywords.put("Text_width" , new BracketCounter("setWidth(", 1));
-        keywords.put("Text_height" , new BracketCounter("setWidth(", 1));
-        keywords.put("Text_text" , new BracketCounter("setText(", 1));
-        keywords.put("Text_text-color" , new BracketCounter("setTextColor(Color.parseColor(", 2));
+        keywords.put("background-color" , new BracketCounter("setBackgroundColor(Color.parseColor(", 2));
+        keywords.put("text" , new BracketCounter("setText(", 1));
+        keywords.put("text-color" , new BracketCounter("setTextColor(Color.parseColor(", 2));
+        keywords.put("gravity" , new BracketCounter("setGravity(GRAVITY.", 1));
+        keywords.put("width" , new BracketCounter("setWidth(", 1));
+        keywords.put("height" , new BracketCounter("setWidth(", 1));
+        keywords.put("orientation" , new BracketCounter("setOrientation(", 1));
     }
    
     public String ToString(Node node){
